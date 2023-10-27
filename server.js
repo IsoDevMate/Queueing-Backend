478d5a5857bea5b710e92887fa018b1e0ef38e22
Barack _ Ouma <127542979+IsoDevMate@users.noreply.github.com>
2023-10-27 10:30:24 +0300

    added folder-structure

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const mongoose=require('mongoose')
+const connectDB=require('./db')
+const port = process.env.PORT || 5050;
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+// connect Database
+connectDB(); 
+
+// apis
+app.use()
+
+
+// test db connection
+
+mongoose.connection.once('open',()=>{
+  console.log(`Connected Successfully to the Database: ${mongoose.connection.name}`)
+  app.listen(port, () => {
+    console.log(`app is running at localhost:${port}`);
+  });
+  })
+
