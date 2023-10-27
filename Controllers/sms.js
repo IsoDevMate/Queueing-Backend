78f46149b4535b2c88e16d0c9de31b62a33b06cb
MylesMburu <ngicumyles@gmail.com>
2023-10-27 13:57:55 +0300

    sms

---
+++ Controllers/sms.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AfricasTalking = require('africastalking')({
+    apiKey: process.env.API_KEY,
+    username: process.env.USERNAME
+});
+
+const sms = AfricasTalking.SMS;
+
+const connectDB = require('./db');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/sms', async (req, res) => {
+    console.log(req.body);
+    const {phoneNumber} = req.body;
+
+    console.log(phoneNumber);
+    const options = {
+        to: [`+254${phoneNumber}`],
+        message: 'Hello world',
+        from: '',
+    }
+    try {
+        await sms.send(options)
+    res.status(200).send({ message: 'Message sent successfully.' })
+    } catch (error) {
+        console.error(err)
+        console.log("Could not send SMS:", error.message);
+    }
+})
--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-const mongoose=require('mongoose')
-const connectDB=async()=>{
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.DATABASE_URL,{
-            useUnifiedTopology:true,
-            useNewUrlParser:true
+        console.log(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.DATABASE_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
         })
+        console.log("Connected to DB")
     } catch (err) {
-        console.error(err)
+        console.error("An error occurred while connecting to the DB:", err.message)
     }
 }
-module.exports=connectDB;
+
+module.exports = connectDB;
