d9923064262bb66cf07a3bde545b4938f3798fda
Barack _ Ouma <127542979+IsoDevMate@users.noreply.github.com>
2023-10-30 15:07:54 +0300

    returned ticket successfully

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,4 +1,4 @@
-
+const Service = require('../models/serviceschema');
 const Ticket = require('../models/ticketschema');
 const bcrypt = require('bcrypt');
 const User =require('../models/schema');
@@ -10,7 +10,7 @@ exports.createUser = async (req, res,next) => {
             res.status(400).send('User already exists...');
             return null; // Return null to indicate an error
         }
-
+ 
         user = new User({
             service:service,
             name: req.body.name,
@@ -21,10 +21,11 @@ exports.createUser = async (req, res,next) => {
         const saltRounds = 10;
         const salt = await bcrypt.genSalt(saltRounds);
         user.email = await bcrypt.hash(user.email, salt);
-
+        console.log("user:",user);
         user = await user.save();
+        req.user = user; // Attach the user document to the request object
         next();
-        return user;
+        return user
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error on user creation ');
@@ -32,14 +33,16 @@ exports.createUser = async (req, res,next) => {
     }
 };
 
-const Service = require('../models/serviceschema');
+//const Service = require('../models/serviceschema');
 
 exports.createTicket = async (req, res) => {
     try {
         const serviceName = req.body.service;
-        const userId = req.body.id;
+       // const service_id = req.body.service_id;
+        const userId = req.user._id; // Get the user ID from the user document attached to the request object
         console.log("user id:",userId);
         console.log("service name:",serviceName);
+        //console.log("service id:",service_id);
         // Check if the user has an active ticket for the same service
         const existingTicket = await Ticket.findOne({
             user_id: userId,
@@ -64,15 +67,17 @@ exports.createTicket = async (req, res) => {
             res.status(400).send('Service not found');
             return null;
         }
-
+         //icket_no, user_id, and ticket_status
         // Create a new ticket
         const ticket = new Ticket({
             user_id: userId,
+            ticket_no: Math.floor(Math.random() * 1000000).toString(), // Generate a random ticket number
             service_id: selectedService._id, // Use the ID of the selected service
             ticket_status: req.body.ticket_status
         });
-
+        console.log("ticket payload:", ticket);
         await ticket.save();
+        console.log("ticket saved! Here is you ticket", ticket);
         return ticket;
 
     } catch (error) {
