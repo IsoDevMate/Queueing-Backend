ea537cef3da656b791dc477ad91d64bc01b94ab4
IsoDevMate <oumaoduor5827@gmail.com>
2023-12-16 07:24:35 +0300

    ...debugging square api error

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -21,146 +21,152 @@ const {
   //const env = process.env["ENVIRONMENT"].toLowerCase();
   //const accessToken = process.env["SQUARE_ACCESS_TOKEN"];
 
-exports.createUser = async (req, res,next) => {
+// Assuming these are the necessary imports, adjust them based on your project structure
+
+
+
+
+exports.createUser = async (req, res, next) => {
     try {
         const service = req.body.service;
-        let user = await User.findOne({ phone: req.body.phoneNumber});
+        let user = await User.findOne({ phone: req.body.phoneNumber });
+
         if (user) {
             res.status(400).send('User already exists...');
             return null; // Return null to indicate an error
         }
-        
+
         user = new User({
-            service:service,
+            service: service,
             name: req.body.name,
             email: req.body.email,
             phoneNumber: req.body.phoneNumber,
         });
-     
+
         try {
             const response = await customersApi.createCustomer({
-              idempotencyKey:  uuidv4(),
-              givenName:user.name,
-              familyName: 'ouma',
-              emailAddress: user.email,
-              phoneNumber: user.phoneNumber,
-              referenceId: 'YOUR_REFERENCE_ID',
-              note: 'a customer'
+                idempotencyKey: uuidv4(),
+                givenName: user.name,
+                familyName: 'ouma',
+                emailAddress: user.email,
+                phoneNumber: user.phoneNumber,
+                referenceId: 'YOUR_REFERENCE_ID',
+                note: 'a customer'
             });
 
-            
             console.log("Customer creation response:", response);
             console.log(response.result);
-            return customer.id;
-          } catch(error) {
+            return response.result.customer.id; // Correct the return statement
+        } catch (error) {
             console.log(error.message);
-          }
+        }
 
         const saltRounds = 10;
         const salt = await bcrypt.genSalt(saltRounds);
         user.email = await bcrypt.hash(user.email, salt);
-       // user.phoneNumber = await bcrypt.hash(user.phoneNumber, salt);
-        console.log("user:",user);
+
         user = await user.save();
-        req.user = user; // Attach the user document to the request object
+        req.user = user;
         next();
         return user;
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error on user creation ');
-        return null; // Return null to indicate an error
+        res.status(500).send('Internal Server Error on user creation');
+        return null;
     }
-   
 };
 
-//const Service = require('../models/serviceschema');
+exports.createTicket = async (req, res, next) => {
+  try {
+      const serviceName = req.body.service;
+      const userId = req.user._id;
 
-exports.createTicket = async (req, res, next, service) => {
-    try {
-        const serviceName = req.body.service;
-        console.log("service name:",serviceName);
-       // const service_id = req.body.service_id;
-        const userId = req.user._id; // Get the user ID from the user document attached to the request object
-        console.log("user id:",userId);
-        console.log("service name:",serviceName);
-        //console.log("service id:",service_id);
-        // Check if the user has an active ticket for the same service
-        const existingTicket = await Ticket.findOne({
-            user_id: userId,
-            service_id: { $ne: null }, // Check for an assigned service ID
-            ticket_status: { $ne: 'completed' }
-        });
-        console.log("ticket content:", existingTicket);
-
-        if (existingTicket) {
-            // Check if the existing ticket is for the same service
-            const existingService = await Service.findById(existingTicket.service_id);
-            if (existingService.name === serviceName) {
-                res.status(400).send('User already has an active ticket for the same service');
-                return null;
-            }
-        }
-
-       // Use the provided 'service' parameter
-  const selectedService = await Service.findOne({ name: service });
-  if (!selectedService) {
-    // Handle the case when the service is not found
-    res.status(400).send('Service not found');
-    return null;
-  }
-
-         //icket_no, user_id, and ticket_status
-        // Create a new ticket
-        const ticket = new Ticket({
-            user_id: userId,
-            ticket_no: Math.floor(Math.random() * 1000000).toString(), // Generate a random ticket number
-            service_id: selectedService._id, // Use the ID of the selected service
-            ticket_status: req.body.ticket_status
-        });
-         
-try{
-    const response = await bookingsApi.createBooking({
-        idempotencyKey: uuidv4(),
-        booking: {
-            startAt: ticket.createdAt,
-            customerId:userId ,
-            customerNote: "Customer's note",
-            sellerNote: 'Complementary VIP service',
-            phoneNumber: req.user.phoneNumber,
-            appointmentSegments: [
-              {
-                durationMinutes: 60,
-                serviceVariationId: 'GUN7HNQBH7ZRARYZN52E7O4B',
-                teamMemberId: '2_uNFkqPYqV-AZB-7neN',
-                serviceVariationVersion: 1604352990016
-              }
-            ],
-          }
-    });
-    
-    console.log(response.result);
-} catch (error) {
-    if (error instanceof ApiError) {
-      error.result.errors.forEach(function (e) {
-        console.log("error caught",e.category);
-        console.log("error to  caught",e.code);
-        console.log("error caught here",e.detail);
+      const existingTicket = await Ticket.findOne({
+          user_id: userId,
+          service_id: { $ne: null },
+          ticket_status: { $ne: 'completed' }
       });
-    } else {
-      console.log("Unexpected error occurred: ", error);
-    }
-  }
-  console.log("ticket payload:", ticket);
-  await ticket.save();
-  console.log("ticket saved! Here is your ticket", ticket);
-  return ticket;
-} catch (error) {
-  console.error(error);
-  res.status(500).send('Internal Server Error on ticket creation');
-  return null;
-}
 
-}
+      if (existingTicket) {
+          const existingService = await Service.findById(existingTicket.service_id);
+          if (existingService.name === serviceName) {
+              res.status(400).send('User already has an active ticket for the same service');
+              return null;
+          }
+      }
+
+      const selectedService = await Service.findOne({ name: serviceName });
+      if (!selectedService) {
+          res.status(400).send('Service not found');
+          return null;
+      }
+
+      let ticket;
+
+      try {
+          // Create a new ticket
+          ticket = new Ticket({
+              user_id: userId,
+              ticket_no: Math.floor(Math.random() * 1000000).toString(),
+              service_id: selectedService._id,
+              ticket_status: req.body.ticket_status
+          });
+
+          // Set the 'createdAt' property before making the booking request
+          ticket.createdAt = new Date(); // Replace this with the actual creation date
+
+          const response = await bookingsApi.createBooking({
+              idempotencyKey: uuidv4(),
+              booking: {
+                  startAt: ticket.createdAt,
+                  customerId: userId,
+                  customerNote: "Customer's note",
+                  sellerNote: 'Complementary VIP service',
+                  phoneNumber: req.user.phoneNumber,
+                  appointmentSegments: [
+                      {
+                          durationMinutes: 60,
+                          serviceVariationId: 'GUN7HNQBH7ZRARYZN52E7O4B',
+                          teamMemberId: '2_uNFkqPYqV-AZB-7neN',
+                          serviceVariationVersion: 1604352990016
+                      }
+                  ],
+              }
+          });
+
+          console.log(response.result);
+
+          console.log("ticket payload:", ticket);
+          await ticket.save();
+          console.log("ticket saved! Here is your ticket", ticket);
+
+          return ticket;
+
+      } catch (error) {
+          if (error instanceof ApiError) {
+              error.result.errors.forEach(function (e) {
+                  console.log("error caught", e.category);
+                  console.log("error to caught", e.code);
+                  console.log("error caught here", e.detail);
+              });
+          } else {
+              console.log("Unexpected error occurred: ", error);
+          }
+
+          // Ensure the response is sent only once
+          return null;
+      }
+
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error on ticket creation');
+      // Ensure the response is sent only once
+      return null;
+  }
+};
+
+
+
 
 exports.callback=async(req,res)=>{
    // const callbackurl=process.env.CALLBACK_URL;
